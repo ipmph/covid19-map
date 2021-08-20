5f7a0b6af9a3fa74c24b0729bf88aebcab865514
ipmph <89189790+ipmph@users.noreply.github.com>
2021-08-20 22:57:55 +0700

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -914,11 +914,6 @@ $(window).on('load', function() {
     } else {
       credit += ' | ';
     }
-
-    credit += 'View <a href="' + getSetting('_githubRepo') + '">code</a>';
-    if (getSetting('_codeCredit')) credit += ' by ' + getSetting('_codeCredit');
-    credit += ' with ';
-    $('.leaflet-control-attribution')[0].innerHTML = credit + attributionHTML;
   }
 
 
